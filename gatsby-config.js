3ef169a4dd5962625d297b78d450f6bd9c10906e
ccontreras <ccontreras@forhims.com>
2020-02-29 11:01:15 -0700

    Remove weird dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -38,6 +36,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    
+
   ],
 }
